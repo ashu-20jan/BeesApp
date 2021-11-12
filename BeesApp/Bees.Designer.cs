09c718e851cb3606f9d4b5473f1caf977f329339
Ashwini Anandathirtha <89642459+ashu-20jan@users.noreply.github.com>
2021-11-12 09:58:11 +0000

    Add files via upload

---
+++ BeesApp/Bees.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace BeesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StatusBtn
+            // 
+            this.StatusBtn.Location = new System.Drawing.Point(12, 78);
+            this.StatusBtn.Name = "StatusBtn";
+            this.StatusBtn.Size = new System.Drawing.Size(123, 47);
+            this.StatusBtn.TabIndex = 0;
+            this.StatusBtn.Text = "Click Here";
+            this.StatusBtn.UseVisualStyleBackColor = true;
+            this.StatusBtn.Click += new System.EventHandler(this.StatusBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(569, 374);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status Of Bees";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 688);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StatusBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StatusBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BeesApp/Bees.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeesApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void StatusBtn_Click(object sender, EventArgs e)
+        {
+            // List to create instance of each type Bee
+            List<Bee> list = new List<Bee>();
+            
+            for (int i = 0; i < 2; i++)
+            {
+                list.Clear();
+                list.Add(new Worker());
+                list.Add(new Drone());
+                list.Add(new Queen());
+            }
+            //To print the status of all the bees
+            void Status(int number, Bee bee)
+            {
+                
+                textBox1.Text += (+number, '"' + bee.Type + '"',"Alive:" , bee.Alive, bee.health).ToString() + "\r\n";
+        }
+
+            //random number generator
+            var random = new Random();
+            for (int i = 0; i < list.Count; i++)
+            {
+                var damage = random.Next(0, 80);
+                list[i].Damage(damage);
+                Status(i + 1, list[i]);
+            }
+        }
+
+       
+    }
+
+
+    // Base Class
+    public  class Bee
+    {
+        //Properties
+        public  float health { get;  set; }
+                     
+    
+        public  string Type { get;  set; }
+        protected int limit { get; set; }
+        public  void Damage(int damage)
+        {
+
+            if ( damage > 0 && damage < 100)
+            {
+                this.health = this.health - damage;
+            }
+            
+        }
+        //Constructor
+        public Bee(string type, int limit)
+        {
+            this.Type = type;
+            this.limit = limit;
+            this.health = 100;
+
+        }
+        public  bool Alive
+        {
+            get
+            {
+                return (this.health > this.limit);
+            }
+        }
+
+
+    }
+    // Derived Class Worker
+    public  class Worker : Bee
+    {
+        public Worker() : base("Worker", 70) { }
+
+
+    }
+    //Derived Class Queen
+    public  class Queen : Bee
+    {
+        public Queen() : base("Queen", 20) { }
+
+
+    }
+    //Derived Class Drone
+    public   class Drone : Bee
+    {
+        public Drone() : base("Drone", 50) { }
+
+
+    }
+}
